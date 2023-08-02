62a5decdcc93f0889f88cb96788e89f5e4146a8d
MarcosAurelio <aurelioleonel@gmail.com>
2023-08-02 16:11:42 -0300

    Incluido template para inserções de eventos na agenda

--- core/views.py
+++ core/views.py
@@ -43,3 +43,20 @@ def lista_eventos(request):
     # evento = Evento.objects.get(id=1)
 
 
+def evento(request):
+    return render(request, 'evento.html')
+
+
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              data_evento=data_evento,
+                              descricao=descricao,
+                              usuario=usuario)
+    return redirect('/')
+
+
